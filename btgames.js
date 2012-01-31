afa19dbb150a3a1e4dda95ac56c20a4a4be33cba
Dawid <dawid.malan@gmail.com>
2012-01-31 22:00:38 +0200

    Will have to wait for one to complete before parsing next

--- btgames.js
+++ btgames.js
@@ -1,21 +1,46 @@
-var underscore = require("./vendor/underscore.js");
+var _ = require("./vendor/underscore.js");
+
+//detect when xhr is complete
+var contentLoaded = function(window) {
+    return window.querySelector("table");   //wow this is lame
+};
+
 
 var btgames = {
     common_tags: ["games"],
     pages: [
-       {url : "http://www.btgames.co.za/bt/release.asp", tags: ["pc"]}     
+        {url : "http://www.btgames.co.za/bt/release.asp", tags: []}     
+    ],
+    platforms: [
+        {value: '1', name: "PC-CD/DVD"},
+        {value: '3', name: "PS3"},
+        {value: '4', name: "3DS"},
+        {value: '8', name : "XBOX 360"},
+        {value: '10', name: "WII"}
     ],
     parser: function(pageTags, callback){
+        var that = this;          
         return function(error, browser){
-            console.log(browser.html());
-            underscore.each( [3], function(val){
-                browser.select("#p", val.toString());             
-                browser.evaluate("getformat()");
-               
-                console.log("After eval");
-                
-                
+            var queue = [],
+                paused = false;
+
+            _.each(that.platforms, function(platform){
+                queue.push(function() {
+                    console.log("Executing " + platform);
+                    browser.select("#p", platform.value); 
+                    browser.evaluate("document.getElementById('selection').submit()");  //do the submit cause zombie is too lame to pick up onchange
+                    browser.wait(contentLoaded, function(){
+                        console.log( browser.html() );
+                    });
+                });
             });
+
+            var runPlatforms = function(){
+                
+            };
+            
+           
+             
         };
     }
 };
